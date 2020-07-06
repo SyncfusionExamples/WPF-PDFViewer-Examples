baba5b6792ef39437bc27c1d9f3f2692e493fb14
DeepakG <deepak21191@gmail.com>
2020-07-06 23:24:04 +0530

    Display PDF file demo is added

---
+++ DisplayPDF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using System.Windows;
+
+namespace PdfViewerDemo
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        # region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
