4390dd95d1b310c6cbe0efa32ff17fc2b4e06755
DeepakG <deepak21191@gmail.com>
2020-10-14 20:12:25 +0530

    Printing demos folder is created and added demo for printing in Windows Service

---
+++ Printing/WindowsServiceApp/PDFPrinter/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceProcess;
+using Syncfusion.Windows.PdfViewer;
+using System.Threading;
+using System.IO;
+
+namespace PDFPrinter
+{
+    public partial class Service1 : ServiceBase
+    {
+        PdfDocumentView pdfViewerControl = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+            System.IO.Directory.SetCurrentDirectory(System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+       
+        protected override void OnStart(string[] args)
+        {
+            // Set the threading model to STA
+            Thread thread = new Thread(PrintPDF);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        // Prints PDF file
+        void PrintPDF()
+        {
+            pdfViewerControl = new PdfDocumentView();
+			// Load the PDF document to be printed
+            pdfViewerControl.Load(@"../../Data/Barcode.pdf");
+			// Print the PDF document silently using the printer name.
+            pdfViewerControl.Print("Pass your printer name here...");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
