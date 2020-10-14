5a633bee9cce55ed267223dbd8e08d9528eedefd
DeepakG <deepak21191@gmail.com>
2020-10-15 00:18:17 +0530

    Printing PDF file in a console application

---
+++ Printing/ConsoleApp/PDFPrinter/Program.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System;
+
+namespace PDFPrinter
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            PdfDocumentView pdfViewer = new PdfDocumentView();
+            pdfViewer.Load(@"../../Data/Barcode.pdf");
+            pdfViewer.Print();
+        }
+    }
+}
