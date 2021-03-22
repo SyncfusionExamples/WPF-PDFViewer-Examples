c32419cbd9c6af085ecbdec15fe955caee8ac412
DeepakG <deepak21191@gmail.com>
2021-03-22 14:27:49 +0530

    Redaction folder and sample are added

---
+++ Redaction-Examples/Find&Redact/App.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+
+namespace PdfViewerDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+    }
+}
