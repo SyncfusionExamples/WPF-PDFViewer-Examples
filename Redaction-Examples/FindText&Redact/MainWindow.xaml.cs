0228af4f624e7ba4144788cac725e50b6fa79caa
DeepakG <deepak21191@gmail.com>
2021-03-22 16:18:44 +0530

    Redaction sample is completed

--- Redaction-Examples/FindText&Redact/MainWindow.xaml.cs
+++ Redaction-Examples/FindText&Redact/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-using System;
-using System.IO;
+using Syncfusion.Windows.PdfViewer;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows;
 
 namespace PdfViewerDemo
@@ -16,54 +17,89 @@ namespace PdfViewerDemo
         }
         #endregion
 
-        int[] GetPages()
+        private void ApplyRedaction_Click(object sender, RoutedEventArgs e)
         {
-            try
+            if (!MarkForRedaction.IsChecked.Value)
             {
-                int[] pageIndexes = null;
-                string[] pageText=  textBox.Text.Split(new char[] {','});
-                pageIndexes = new int[pageText.Length];
-                for(int i=0;i<pageText.Length;i++)
-                {
-                    pageIndexes[i] = int.Parse(pageText[i].Trim());
-                }
-                return pageIndexes;
+                Dictionary<int, List<RectangleF>> textBounds = GetTextBounds(textBox.Text);
+                MarkRegions(textBounds);
             }
-            catch (Exception exception)
-            {
-                string message = "Error Occurred: " + exception.Message + "\n";
-                message += "Plese provide page numbers in the required format";
-                MessageBox.Show(message);
-                return null;
-            }
+            pdfViewer.PageRedactor.ApplyRedaction();
+            textBox.Clear();
         }
 
-        private void Organize_Click(object sender, RoutedEventArgs e)
+        Dictionary<int, List<RectangleF>> GetTextBounds(string text)
         {
-            int[] pages = GetPages();
-            if (pages != null)
+            text = text.ToLower();
+            Dictionary<int, List<RectangleF>> textBounds = new Dictionary<int, List<RectangleF>>();
+
+            for (int i = 0; i < pdfViewer.PageCount; i++)
             {
-                if (Clockwise.IsChecked.Value)
-                    pdfViewer.PageOrganizer.RotateClockwise(pages);
-                else if (Counterclockwise.IsChecked.Value)
-                    pdfViewer.PageOrganizer.RotateCounterclockwise(pages);
-                else if (Remove.IsChecked.Value)
+                List<RectangleF> bounds = new List<RectangleF>();
+
+                List<TextData> textDataCollection = new List<TextData>();
+                string extractedText = pdfViewer.ExtractText(i, out textDataCollection).ToLower();
+
+                int start = 0;
+                int at = 0;
+                int end = extractedText.Length;
+                int count = 0;
+
+                while ((start <= end) && (at > -1))
                 {
-                    if (pages.Length < pdfViewer.PageCount)
-                        pdfViewer.PageOrganizer.RemovePages(pages);
-                    else
-                        MessageBox.Show("Selected pages count should be less than that of the orginal file");
+                    count = end - start;
+                    at = extractedText.IndexOf(text, start, count);
+                    if (at == -1)
+                        break;                  
+
+                    RectangleF startBounds = textDataCollection[at].Bounds;
+                    RectangleF endBounds = textDataCollection[at + text.Length - 1].Bounds;
+
+                    RectangleF rectangle = new RectangleF(startBounds.X, startBounds.Y,
+                        endBounds.X - startBounds.X + endBounds.Width,
+                        startBounds.Height > endBounds.Height ? startBounds.Height : endBounds.Height);
+                    bounds.Add(rectangle);
+
+                    start = at + text.Length;
                 }
-                else if (Rearrange.IsChecked.Value)
-                {
-                    if (pages.Length == pdfViewer.PageCount)
-                        pdfViewer.PageOrganizer.ReArrange(pages);
-                    else
-                        MessageBox.Show("Pages count should be equal to that of original file");
-                }
-                if (Save.IsChecked.Value)
-                    pdfViewer.Save("Organized.pdf");
+                textBounds.Add(i, bounds);
             }
+            return textBounds;
+        }
+
+        void MarkRegions(Dictionary<int, List<RectangleF>> textBounds)
+        {
+            for (int i = 0; i < pdfViewer.PageCount; i++)
+            {
+                pdfViewer.PageRedactor.MarkRegions(i, textBounds[i]);
+            }
+            pdfViewer.PageRedactor.EnableRedactionMode = true;
+        }
+
+        void ClearRegions()
+        {
+            for (int i = 0; i < pdfViewer.PageCount; i++)
+            {
+                pdfViewer.PageRedactor.MarkRegions(i, new List<RectangleF>());
+            }
+            pdfViewer.PageRedactor.EnableRedactionMode = true;
+        }
+
+        private void MarkForRedaction_Checked(object sender, RoutedEventArgs e)
+        {
+            Dictionary<int, List<RectangleF>> textBounds = GetTextBounds(textBox.Text);
+            MarkRegions(textBounds);
+        }
+
+        private void MarkForRedaction_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ClearRegions();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            pdfViewer.Save("Redacted.Pdf");
+            MessageBox.Show("The document is saved in the application folder");
         }
     }
 }
