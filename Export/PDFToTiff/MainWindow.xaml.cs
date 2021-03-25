7523f035389d2ae2218c3c934b653480be9baefa
DeepakG <deepak21191@gmail.com>
2021-03-25 15:59:53 +0530

    Folder path is changed

--- Export/PDFToTiff/MainWindow.xaml.cs
+++ Export/PDFToTiff/MainWindow.xaml.cs
@@ -28,8 +28,6 @@ namespace PDFToTIFFSample
             pdfViewer.Load(fileName);
             //Export the images From the input PDF file at the page range of 0 to 1 .
             BitmapSource[] image = pdfViewer.ExportAsImage(0, pdfViewer.PageCount - 1);
-            //Setup the output path
-            string output = @"..\..\Output\Image";
             if (image != null)
             {
                 for (int i = 0; i < image.Length; i++)
@@ -41,7 +39,7 @@ namespace PDFToTIFFSample
                     //Create the bitmap frame using the bitmap source and add it to the encoder.
                     encoder.Frames.Add(BitmapFrame.Create(image[i]));
                     //Create the file stream for the output in the desired image format.
-                    using (FileStream stream = new FileStream(output + i.ToString() + ".Tiff", FileMode.Create))
+                    using (FileStream stream = new FileStream("Image_" + i.ToString() + ".Tiff", FileMode.Create))
                     {
                         //Save the stream, so that the image will be generated in the output location.
                         encoder.Save(stream);
