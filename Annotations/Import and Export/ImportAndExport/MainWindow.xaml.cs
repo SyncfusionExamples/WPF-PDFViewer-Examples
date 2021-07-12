e6dfab6ae9dbb0cc12cb9d323f9b7647722d3fc0
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2021-07-12 16:38:39 +0530

    Add files via upload
    Import and Export Annotations example is added

---
+++ Annotations/Import and Export/ImportAndExport/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.Pdf.Parsing;
+using System.Windows;
+
+namespace PdfViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PdfLoadedDocument document = new PdfLoadedDocument(@"../../Data/BlankPage.pdf");
+            pdfViewer.Load(document);
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            if (fdfRadioButton.IsChecked.Value)
+                openFileDlg.Filter = "FDF Documents|*.fdf";
+            else
+                openFileDlg.Filter = "XFDF Documents|*.xfdf";
+            if (openFileDlg.ShowDialog().Value)
+            {
+                AnnotationDataFormat annotationDataFormat;
+                if (fdfRadioButton.IsChecked.Value)
+                    annotationDataFormat = AnnotationDataFormat.Fdf;
+                else
+                    annotationDataFormat = AnnotationDataFormat.XFdf;
+                pdfViewer.ImportAnnotations(openFileDlg.FileName, annotationDataFormat);
+            }
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDlg = new Microsoft.Win32.SaveFileDialog();
+            if (fdfRadioButton.IsChecked.Value)
+                saveFileDlg.Filter = "FDF Documents|*.fdf";
+            else
+                saveFileDlg.Filter = "XFDF Documents|*.xfdf";
+            if (saveFileDlg.ShowDialog().Value)
+            {
+                AnnotationDataFormat annotationDataFormat;
+                if (fdfRadioButton.IsChecked.Value)
+                    annotationDataFormat = AnnotationDataFormat.Fdf;
+                else
+                    annotationDataFormat = AnnotationDataFormat.XFdf;
+                pdfViewer.ExportAnnotations(saveFileDlg.FileName, annotationDataFormat);
+            }
+        }
+    }
+}
