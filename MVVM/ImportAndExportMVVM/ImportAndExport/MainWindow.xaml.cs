8f31b453858d413323d8267d8249fb8399678783
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2021-08-02 18:17:57 +0530

    Added MVVM example project

---
+++ MVVM/ImportAndExportMVVM/ImportAndExport/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Pdf.Parsing;
+using System.Windows;
+
+namespace PdfViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVM/ImportAndExportMVVM/ImportAndExport/ViewModel/ViewModel.cs
@@ -0,0 +1,113 @@
+﻿using Syncfusion.Pdf.Parsing;
+using Syncfusion.Windows.PdfViewer;
+using Syncfusion.Windows.Shared;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PdfViewer
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Stream documentStream;
+        private int formatIndex = 1;
+        ICommand exportAnnotationsCommand;
+        ICommand importAnnotationsCommand;
+
+        public ViewModel()
+        {
+            documentStream = new FileStream(@"..\..\Data\Simple Shapes.pdf", FileMode.OpenOrCreate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Stream DocumentStream
+        {
+            get
+            {
+                return documentStream;
+            }
+            set
+            {
+                documentStream = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("DocumentStream"));
+            }
+        }
+
+        public int FormatIndex
+        {
+            get
+            {
+                return formatIndex;
+            }
+            set
+            {
+                formatIndex = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("FormatIndex"));
+            }
+        }
+
+        public ICommand ExportAnnotationsCommand
+        {
+            get
+            {
+                if (exportAnnotationsCommand == null)
+                {
+                    exportAnnotationsCommand = new DelegateCommand<PdfViewerControl>(ExportAnnotations);
+                }
+                return exportAnnotationsCommand;
+            }
+        }
+
+        public ICommand ImportAnnotationsCommand
+        {
+            get
+            {
+                if (importAnnotationsCommand == null)
+                {
+                    importAnnotationsCommand = new DelegateCommand<PdfViewerControl>(ImportAnnotations);
+                }
+                return importAnnotationsCommand;
+            }
+        }
+
+        void ExportAnnotations(PdfViewerControl pdfViewerControl)
+        {
+            AnnotationDataFormat annotationDataFormat;
+            if (FormatIndex == 0)
+            {
+                annotationDataFormat = AnnotationDataFormat.Fdf;              
+                pdfViewerControl.ExportAnnotations(@"../../Data/Exported Annotations.fdf", annotationDataFormat);
+            }
+            else
+            {
+                annotationDataFormat = AnnotationDataFormat.XFdf;
+                pdfViewerControl.ExportAnnotations(@"../../Data/Exported Annotations.xfdf", annotationDataFormat);
+            }
+            
+        }
+
+        void ImportAnnotations(PdfViewerControl pdfViewerControl)
+        {
+            AnnotationDataFormat annotationDataFormat;
+            if (FormatIndex == 0)
+            {
+                annotationDataFormat = AnnotationDataFormat.Fdf;
+                pdfViewerControl.ImportAnnotations(@"../../Data/Annotations.fdf", annotationDataFormat);
+            }
+            else
+            {
+                annotationDataFormat = AnnotationDataFormat.XFdf;
+                pdfViewerControl.ImportAnnotations(@"../../Data/Annotations.xfdf", annotationDataFormat);
+            }
+            
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+    }
+}
