db9eea8a1f89a8525e6f359e099a08c947c0a1b3
Deepak G <65169077+Deepak1799@users.noreply.github.com>
2021-09-03 17:14:43 +0530

    Get selected pages from PDF

---
+++ OrganzingPages-Examples/Get Selected pages information/PdfViewer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+
+namespace PdfViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            pdfViewer.PageSelected += PdfViewer_PageSelected;
+            pdfViewer.Load(@"../../Data/PDF_Succinctly.pdf");
+        }
+
+        private void PdfViewer_PageSelected(object sender, PageSelectedEventArgs e)
+        {
+            string selectedPages = string.Empty;
+            for(int i=0;i<e.SelectedPages.Length;i++)
+            {
+                selectedPages += (e.SelectedPages[i]+1).ToString();
+                selectedPages += " ";
+            }
+            SelectedPagesTextBlock.Text = "The selected pages are: " + selectedPages.ToString();
+        }
+    }
+}
---
+++ OrganzingPages-Examples/Get Selected pages information/README.md
@@ -0,0 +1,5 @@
+# Get selected pages information using WPF PDF Viewer
+Organize pages support in Syncfusion WPF PDF Viewer allows you to select pages from a PDF document and retrieve the selected pages information using a miniature preview of the PDF pages.
+
+## User Guide reference
+[Select pages and get selected pages information](https://help.syncfusion.com/wpf/pdf-viewer/organize-pages#get-the-selected-page-indexes)
