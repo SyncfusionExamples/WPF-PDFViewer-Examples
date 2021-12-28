0cdd5eacaae91ec260af1baf252fb2c950359f52
DeepakG <deepakgunasekaran@syncfusion.com>
2021-12-28 23:44:36 +0530

    Added example for hiding annotations in the PDF file

---
+++ Annotations/HideComments/CheckedStateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HideComments
+{
+    internal class CheckedStateConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(bool)values[0] || !(bool)values[1] || !(bool)values[2])
+                return false;
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            object[] values = null;
+            if ((bool)value)
+                values = new object[3] {true, true, true};
+            else
+                values = new object[3] { false, false, false };
+            return values;
+        }
+    }
+}
---
+++ Annotations/HideComments/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HideComments
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pdfViewer.Load("../../Data/Annotations.pdf");
+        }
+
+        private void User_Checked(object sender, RoutedEventArgs e)
+        {
+            if (pdfViewer != null)
+            {
+                CheckBox checkBox = (CheckBox)sender;
+                PdfLoadedDocument pdfLoadedDocument = pdfViewer.LoadedDocument;
+                for (int i = 0; i < pdfLoadedDocument.Pages.Count; i++)
+                {
+                    for (int j = 0; j < pdfLoadedDocument.Pages[i].Annotations.Count; j++)
+                    {
+                        var annotation = pdfLoadedDocument.Pages[i].Annotations[j];
+                        if (annotation.Name.Contains(checkBox.Name))
+                            pdfViewer.ShowAnnotation(annotation.Name);
+                    }
+                }
+            }
+        }
+
+        private void User_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (pdfViewer != null)
+            {
+                CheckBox checkBox = (CheckBox)sender;
+                PdfLoadedDocument pdfLoadedDocument = pdfViewer.LoadedDocument;
+                for (int i = 0; i < pdfLoadedDocument.Pages.Count; i++)
+                {
+                    for (int j = 0; j < pdfLoadedDocument.Pages[i].Annotations.Count; j++)
+                    {
+                        var annotation = pdfLoadedDocument.Pages[i].Annotations[j];
+                        if (annotation.Name.Contains(checkBox.Name))
+                            pdfViewer.HideAnnotation(annotation.Name);
+                    }
+                }
+            }
+        }
+    }
+}
