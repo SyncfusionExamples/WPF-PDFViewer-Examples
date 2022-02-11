69c508976612be6fb6580c6d1163a3042805c29e
Rajavignesh Bala Sankar <rajavignesh.b@syncfusion.com>
2022-02-11 21:17:56 +0530

    Feedback Addressed

---
+++ Annotations/AddAnnotation/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+
+namespace WpfPDFViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Loads the document in PDF Viewer
+            pdfViewer.Load("../../F Sharp Succinctly.pdf");
+        }
+
+        private void Add_Ink_Click(object sender, RoutedEventArgs e)
+        {
+            //Gets the loadedDocument from PDF Viewer
+            PdfLoadedDocument loadedDocument = pdfViewer.LoadedDocument;
+
+            //Specifies the ink points
+            List<float> linePoints = new List<float> { 40, 300, 60, 100, 40, 50, 40, 300 };
+
+            //Specifies the bounds of an annotation
+            RectangleF rectangle = new RectangleF(0, 0, 300, 400);
+
+            //Creates a new ink annotation
+            PdfInkAnnotation inkAnnotation = new PdfInkAnnotation(rectangle, linePoints);
+
+            inkAnnotation.Color = new PdfColor(System.Drawing.Color.Red);
+
+            //Adds annotation to the page
+            loadedDocument.Pages[0].Annotations.Add(inkAnnotation);
+
+            //Creates new memory stream
+            MemoryStream stream = new MemoryStream();
+
+            //Save the loadedDocument as stream
+            loadedDocument.Save(stream);
+
+            //Load the stream in PDF Viewer
+            pdfViewer.Load(stream);
+        }
+    }
+}
