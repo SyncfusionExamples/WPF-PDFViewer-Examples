0b48578e7bede5226e1cc3f309c278b715e8cfc4
Divya <divya.dhayalan@syncfusion.com>
2022-05-06 11:24:17 +0530

    WPF-53871 - Toggling Layers from code behind

---
+++ Layers/ToggleLayers/ToggleLayers/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System.Windows;
+
+namespace ToggleLayers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pdfViewerControl.Load("../../../../../PDF/LayersDocument.pdf");
+        }
+
+        private void ToggleLayer_Click(object sender, RoutedEventArgs e)
+        {
+            //Retrieves a PDF document's layers collection using PdfViewerControl
+            LayerCollection layers = pdfViewerControl.Layers;
+
+            // Gets a layer by its name                
+            for (int i = 0; i < layers.Count; i++)
+            {
+                if (layers[i].Name == "Layer2")
+                {
+                    //Toggle the visibility of the Layer 
+                    if (layers[i].IsVisible)
+                        layers[i].IsVisible = false;
+                    else
+                        layers[i].IsVisible = true;
+                }
+            }
+        }
+    }
+}
