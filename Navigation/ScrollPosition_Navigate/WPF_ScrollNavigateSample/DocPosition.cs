d66984ee831dc5d31d08c833d5f443c6e7a82f35
Salman Baris <salmanbaris.s@syncfusion.com>
2022-09-29 16:01:13 +0530

    WPF-59328 How to store scrolled position and navigate to the scrolled position

---
+++ Navigation/ScrollPosition_Navigate/WPF_ScrollNavigateSample/DocPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ScrollNavigateSample
+{
+    class DocPosition
+    {
+        public double Horizontal { get; set; }
+
+        public double Vertical { get; set; }
+
+        public int ZoomPercent { get; set; }
+
+        public DocPosition(int zoompercentage, double horizontal, double vertical)
+        {
+            Horizontal = horizontal;
+            Vertical = vertical;
+            ZoomPercent = zoompercentage;
+        }
+    }
+}
