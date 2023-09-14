fed317ef6115a89606df02a4ecd079746e03852b
Krithika <krithika.ganesan@syncfusion.com>
2023-09-14 18:57:39 +0530

    PdfToImageConverter

---
+++ PDF-to-image/PDF-to-image-in-.NET/PDF_to_Image_Core/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDF_to_Image_Core.Models;
+using System.Diagnostics;
+using Syncfusion.PdfToImageConverter;
+using Microsoft.AspNetCore.Hosting.Server;
+
+namespace PDF_to_Image_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webEnvironment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webEnvironment)
+        {
+            _logger = logger;
+            _webEnvironment = webEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConvertToImage(MyPdfUploadViewModel model)
+        {
+            if (model.PdfFile != null && model.PdfFile.Length > 0)
+            {
+                string filePath = Path.Combine(_webEnvironment.WebRootPath, "Sample.pdf");
+
+                using (var stream = new FileStream(filePath, FileMode.OpenOrCreate))
+                {
+                    await model.PdfFile.CopyToAsync(stream);
+                    PdfToImageConverter pdfToImageConverter = new PdfToImageConverter();
+                    pdfToImageConverter.Load(stream);
+                    Stream imageStream = pdfToImageConverter.Convert(0, false, false);
+                    imageStream.Position = 0;
+                    FileStreamResult fileStreamResult = new FileStreamResult(imageStream, "image/png");
+                    fileStreamResult.FileDownloadName = "Sample.png";
+                    return fileStreamResult;
+                }
+
+                return RedirectToAction("Index"); // Redirect to the upload page
+            }
+            else
+            {
+                // Handle invalid file or no file selected
+                ViewBag.Message = "Please select a valid PDF file.";
+                return View("Index");
+            }
+        }
+        private IActionResult File(object value, object png, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- PDF-to-image/PDF-to-image-in-Windows/MainWindow.xaml.cs
+++ PDF-to-image/PDF-to-image-in-Windows/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using Syncfusion.PdfToImageConverter;
+using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows;
 using System.Windows.Media;
@@ -27,7 +29,7 @@ namespace Convert_PDF_page_into_image
             {
                 filePath = openFileDialog.FileName;
                 if(filePath != string.Empty)
-                    convert.IsEnabled= true;
+                    convert.IsEnabled = true;
             }
         }
 
@@ -35,17 +37,33 @@ namespace Convert_PDF_page_into_image
         private void convert_Click(object sender, RoutedEventArgs e)
         {
             FileStream inputPDFStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
-            PdfToImageConverter pdfToImageConvertor = new PdfToImageConverter(inputPDFStream);
-            Stream image = pdfToImageConvertor.Convert(0, false, false);
-            if (image != null)
+            PdfToImageConverter imageConverter = new PdfToImageConverter(inputPDFStream);
+            Stream[] outputStream = new Stream[imageConverter.PageCount];
+            // Convert all the PDF pages to images
+            if (imageConverter.PageCount > 1)
+            {
+                outputStream = imageConverter.Convert(0, imageConverter.PageCount - 1, false, false);
+            }
+            else if (imageConverter.PageCount == 1)
+            {
+                outputStream[0] = imageConverter.Convert(0, false, false);
+            }
+            if (outputStream != null)
             {
                 if (!Directory.Exists("PdfToImage"))
                 {
                     Directory.CreateDirectory("PdfToImage");
                 }
-                Bitmap bitmap = new Bitmap(image);
-                bitmap.Save(@"PdfToImage\output.png");
-                pdfToImageConvertor.Dispose();
+                foreach (Stream stream in outputStream)
+                {
+                    if (stream != null)
+                    {
+                        Bitmap bitmap = new Bitmap(stream);
+                        bitmap.Save(@"PdfToImage\Image" + Guid.NewGuid().ToString() + ".png", ImageFormat.Png);
+                    }
+                }
+                convert.IsEnabled = false;
+                imageConverter.Dispose();
                 if(MessageBox.Show("Do you want to view the converted image?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
                 {
                     //Launching the PDF file using the default Application.[Acrobat Reader]
