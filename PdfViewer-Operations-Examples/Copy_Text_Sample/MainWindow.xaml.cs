aed226b6696b66553ec77be0acfd9e3e6171f3c3
RajaVigneshB <rajavignesh.b@syncfusion.com>
2024-06-11 20:01:41 +0530

    Added Samples for the search and copy text

---
+++ PdfViewer-Operations-Examples/Copy_Text_Sample/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Copy_Text_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            pdfviewer.Load("../../Data/Fsharp_Succinctly.pdf");
+            pdfviewer.GotoPage(2);
+        }
+    }
+}
