45151719c6a6adaa57da4886ce49ccb6b206070d
YathavakrishnanM <yathavakrishnanm.sf4659@gmail.com>
2024-08-16 17:01:07 +0530

    902445 - Need to add playground for pdftoimage converter for crossplatform in UG documentation

---
+++ PDF-to-image/.NET/PDFPage-to-Image/PDFPage-to-Image/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.PdfToImageConverter;
+using System.IO;
+
+
+namespace NET
+{
+    class Programe
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize PDF to Image converter.
+            PdfToImageConverter imageConverter = new PdfToImageConverter();
+
+            //Load the PDF document as a stream
+            FileStream inputStream = new FileStream("../../../Data/Input.pdf", FileMode.Open, FileAccess.ReadWrite);
+            imageConverter.Load(inputStream);
+
+            //Convert PDF to Image.
+            Stream outputStream = imageConverter.Convert(0, false, false);
+
+            //Create file stream.
+            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.jpeg"), FileMode.Create, FileAccess.ReadWrite))
+            {
+                //Save the image to file stream.
+                inputStream.CopyTo(outputFileStream);
+            }
+        }
+    }
+}
