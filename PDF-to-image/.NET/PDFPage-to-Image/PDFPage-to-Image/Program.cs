60caf923ea57d32ed95e24c91501a40b242201f8
YathavakrishnanM <yathavakrishnanm.sf4659@gmail.com>
2024-08-19 18:01:32 +0530

    Feedback changes

--- PDF-to-image/.NET/PDFPage-to-Image/PDFPage-to-Image/Program.cs
+++ PDF-to-image/.NET/PDFPage-to-Image/PDFPage-to-Image/Program.cs
@@ -12,14 +12,14 @@ namespace NET
             PdfToImageConverter imageConverter = new PdfToImageConverter();
 
             //Load the PDF document as a stream
-            FileStream inputStream = new FileStream("../../../Data/Input.pdf", FileMode.Open, FileAccess.ReadWrite);
+            FileStream inputStream = new FileStream(Path.GetFullPath(@"Data/Input.pdf"), FileMode.Open, FileAccess.ReadWrite);
             imageConverter.Load(inputStream);
 
             //Convert PDF to Image.
             Stream outputStream = imageConverter.Convert(0, false, false);
 
             //Create file stream.
-            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"../../../Output/Output.jpeg"), FileMode.Create, FileAccess.ReadWrite))
+            using (FileStream outputFileStream = new FileStream(Path.GetFullPath(@"Output/Output.jpeg"), FileMode.Create, FileAccess.ReadWrite))
             {
                 //Save the image to file stream.
                 inputStream.CopyTo(outputFileStream);
