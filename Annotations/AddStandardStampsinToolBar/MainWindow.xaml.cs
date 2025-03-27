a64b7c812941c2369420651d77a13cd27c515655
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-03-27 20:07:10 +0530

    addressed the feedbacks

--- Annotations/AddStandardStampsinToolBar/MainWindow.xaml.cs
+++ Annotations/AddStandardStampsinToolBar/MainWindow.xaml.cs
@@ -21,29 +21,31 @@ namespace AddingStandardStampsInToolBar
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path;
         public MainWindow()
         {
             InitializeComponent();
             this.WindowState = WindowState.Maximized;
-            //Loads the Document
+            //Setting the path based on the framework
 #if NETFRAMEWORK
-            pdfViewer.Load("../../Data/F#.pdf");
+            path = "../../Data/";
 #else
-            pdfViewer.Load("../../../Data/F#.pdf");
+            path = "../../../Data/";
 #endif
+            pdfViewer.Load(System.IO.Path.Combine(path, "F#.pdf"));
         }
-
+        /// <summary>
+        /// A button click event that creates a stamp from a local image and adds the standard stamp to the toolbar.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event data associated with the click event.</param>
         private void addStamp_Click(object sender, RoutedEventArgs e)
         {
             // Clear the existing standard stamps if it is not needed.
             pdfViewer.ToolbarSettings.StampAnnotations.Clear();
             //Load the custom image from the local disk.
             System.Windows.Controls.Image image = new System.Windows.Controls.Image();
-#if NETFRAMEWORK
-            image.Source = new BitmapImage(new Uri(@"../../Data/Adventure_Cycle.jpg", UriKind.RelativeOrAbsolute));
-#else
-            image.Source = new BitmapImage(new Uri(@"../../../Data/Adventure_Cycle.jpg", UriKind.RelativeOrAbsolute));
-#endif
+            image.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path,"Adventure_Cycle.jpg"), UriKind.RelativeOrAbsolute));
             //Create a new standard stamp from the image.
             PdfStampAnnotation newStandardStamp = new PdfStampAnnotation(image);
             //Add the custom stamp in the toolbar.
--- Annotations/AddStandardStampsinToolBar/Properties/AssemblyInfo.cs
+++ Annotations/AddStandardStampsinToolBar/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AddingStandardStampsInToolBar")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AddingStandardStampsInToolBar")]
+//[assembly: AssemblyTitle("AddingStandardStampsInToolBar")]
+//[assembly: AssemblyDescription("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("AddingStandardStampsInToolBar")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
