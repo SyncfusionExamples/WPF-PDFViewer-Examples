409388a8df0f230a53d1256afe4911b84861e168
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-03-28 11:26:47 +0530

    feedbacks addressed

--- Annotations/AddStandardStampsinToolBar/MainWindow.xaml.cs
+++ Annotations/AddStandardStampsinToolBar/MainWindow.xaml.cs
@@ -33,23 +33,29 @@ namespace AddingStandardStampsInToolBar
             path = "../../../Data/";
 #endif
             pdfViewer.Load(System.IO.Path.Combine(path, "F#.pdf"));
+            pdfViewer.DocumentLoaded += PdfViewer_DocumentLoaded;
         }
-        /// <summary>
-        /// A button click event that creates a stamp from a local image and adds the standard stamp to the toolbar.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">Event data associated with the click event.</param>
-        private void addStamp_Click(object sender, RoutedEventArgs e)
-        {
-            // Clear the existing standard stamps if it is not needed.
-            pdfViewer.ToolbarSettings.StampAnnotations.Clear();
-            //Load the custom image from the local disk.
-            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
-            image.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path,"Adventure_Cycle.jpg"), UriKind.RelativeOrAbsolute));
-            //Create a new standard stamp from the image.
-            PdfStampAnnotation newStandardStamp = new PdfStampAnnotation(image);
-            //Add the custom stamp in the toolbar.
-            pdfViewer.ToolbarSettings.StampAnnotations.Add(newStandardStamp);
-        }
+
+            /// <summary>
+            /// Document loaded event that handles the creation of stamps from the local images and adds the standard stamps to the toolbar.
+            /// </summary>
+            /// <param name="sender">Source of the event</param>
+            /// <param name="args">Event arguments</param>
+            private void PdfViewer_DocumentLoaded(object sender, EventArgs args)
+            {
+                // Clear the existing standard stamps if it is not needed.
+                pdfViewer.ToolbarSettings.StampAnnotations.Clear();
+                //Load the custom image from the local disk.
+                System.Windows.Controls.Image image1 = new System.Windows.Controls.Image();
+                image1.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path, "Adventure_Cycle.jpg"), UriKind.RelativeOrAbsolute));
+                System.Windows.Controls.Image image2 = new System.Windows.Controls.Image();
+                image2.Source = new BitmapImage(new Uri(System.IO.Path.Combine(path, "Confidential.png"), UriKind.RelativeOrAbsolute));
+                //Create a new standard stamp from the image.
+                PdfStampAnnotation newStandardStamp1 = new PdfStampAnnotation(image1);
+                PdfStampAnnotation newStandardStamp2 = new PdfStampAnnotation(image2);
+                //Add the custom stamp in the toolbar.
+                pdfViewer.ToolbarSettings.StampAnnotations.Add(newStandardStamp1);
+                pdfViewer.ToolbarSettings.StampAnnotations.Add(newStandardStamp2 );
+            }
     }
 }
