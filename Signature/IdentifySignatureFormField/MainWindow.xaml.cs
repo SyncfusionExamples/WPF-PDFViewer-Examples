917e6775518a4c031d1796bae37d1d72ce6d6fce
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-06-25 15:52:36 +0530

    939396 - Added sample to identify signatureFormField

---
+++ Signature/IdentifySignatureFormField/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IdentifySignatureFormField
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filePath;
+        public List<String> NameList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+#if NETCOREAPP
+            filePath = @"../../../Data/FormFields.pdf";
+#else
+            filePath = @"../../Data/FormFields.pdf";
+#endif
+            pdfViewer.Load(filePath);
+            pdfViewer.FormFieldClicked += PdfViewer_FormFieldClicked;
+        }
+
+        private void PdfViewer_FormFieldClicked(object sender, Syncfusion.Windows.PdfViewer.FormFieldClickedEventArgs args)
+        {
+            if (args.FormField is TextBox)
+            {
+                //Typecast the `FormField` property to `System.Windows.Controls.TextBox`
+                TextBox text = args.FormField as TextBox;
+                if(NameList.Contains(text.Name))
+                {
+                    MessageBox.Show("SignatureFormField Clicked");
+                }
+            }
+        }
+
+        private void pdfViewer_DocumentLoaded(object sender, EventArgs args)
+        {
+            NameList = new List<String>();
+            PdfLoadedDocument document = new PdfLoadedDocument(filePath);
+            //Get the loaded form.
+            PdfLoadedForm loadedForm = document.Form;
+            for (int i = 0; i < loadedForm.Fields.Count; i++)
+            {
+                if (loadedForm.Fields[i] is PdfLoadedSignatureField)
+                {
+                    NameList.Add(loadedForm.Fields[i].Name);
+                }
+            }
+        }
+
+        private void pdfViewer_DocumentUnloaded(object sender, EventArgs e)
+        {
+            NameList.Clear();
+        }
+    }
+}
