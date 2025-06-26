2bcf1f73bfe4e6e9529637d08b06c76bf3b8b8fe
DhileebanRaja <dhileebanraja.a@syncfusion.com>
2025-06-26 18:47:35 +0530

    feedbacks addressed

--- Signature/Validate-Signature-FormField/MainWindow.xaml.cs
+++ Signature/Validate-Signature-FormField/MainWindow.xaml.cs
@@ -1,22 +1,6 @@
 ﻿using Syncfusion.Pdf.Interactive;
 using Syncfusion.Pdf.Parsing;
-using Syncfusion.Pdf;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Drawing;
 
 namespace CheckPDFSignatureformFieldsSigned
 {
@@ -25,58 +9,13 @@ namespace CheckPDFSignatureformFieldsSigned
     /// </summary>
     public partial class MainWindow : Window
     {
-        string inputFilePath, outputFilePath;
         public MainWindow()
         {
             InitializeComponent();
+            pdfViewer.Load(@"../../../Data/Doc_with_sign.pdf");
         }
 
-        public void LoadDocument(string inputFile, string outputFile)
-        {
-            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(inputFile);
-
-            var pages = loadedDocument.Pages;
-            PdfLoadedPage page = pages[0] as PdfLoadedPage;
-
-            PdfSignatureField signature = new PdfSignatureField(page, "Signature");
-            signature.Bounds = new RectangleF(155, 590, 150, 24);
-            signature.ToolTip = "Sign Here";
-            loadedDocument.Form.Fields.Add(signature);
-
-            PdfTextBoxField vehicleEntry = new PdfTextBoxField(page, "VehicleEntry");
-            vehicleEntry.Bounds = new RectangleF(128, 400, 75, 24);
-            vehicleEntry.BorderColor = new Syncfusion.Pdf.Graphics.PdfColor(0, 1, 1, 0);
-            //Add the form field to the document.
-            loadedDocument.Form.Fields.Add(vehicleEntry);
-
-            PdfSignatureField initials = new PdfSignatureField(page, "Initials");
-            initials.Bounds = new RectangleF(40, 775, 75, 24);
-            initials.ToolTip = "Initials Here";
-            loadedDocument.Form.Fields.Add(initials);
-
-            using (FileStream output = new FileStream(outputFile, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                loadedDocument.Save(output);
-            }
-
-            pdfViewer.Load(outputFile);
-            pdfViewer.ZoomMode = Syncfusion.Windows.PdfViewer.ZoomMode.FitPage;
-        }
-
-        private void btnSignature_Click(object sender, RoutedEventArgs e)
-        {
-#if NETCOREAPP
-            inputFilePath = @"../../../Data/1.pdf";
-            outputFilePath = @"../../../Data/1-output.pdf";
-#else
-            inputFilePath = @"../../Data/1.pdf";
-            outputFilePath = @"../../Data/1-output.pdf";
-#endif
-            LoadDocument(inputFilePath, outputFilePath);
-            
-        }
-
-        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        private void validateSignature_Click(object sender, RoutedEventArgs e)
         {
             bool isSigned = false;
             PdfLoadedDocument loadedDocument = pdfViewer.LoadedDocument;
@@ -137,7 +76,7 @@ namespace CheckPDFSignatureformFieldsSigned
                 }
                 else
                 {
-                    MessageBox.Show("All signatures are completed!");
+                    MessageBox.Show("All signatures fields are signed!");
                 }
             }
         }
