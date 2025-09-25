fea70455c661ea7d767e5bf16a75ebb5b815fcdc
KrithikaGanesan <krithika.ganesan@syncfusion.com>
2025-09-25 13:34:20 +0530

    890909: Revamp the contents of the Import/Export annotations and forms in PdfViewer FT

---
+++ FormFields/Export&ImportFormFields/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+
+namespace Export_ImportFormFields
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filePath = "../../../FormFillingDocument.pdf";
+        public MainWindow()
+        {
+            InitializeComponent();
+            pdfViewer.Load(filePath);
+        }
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            if (fdfRadioButton.IsChecked.Value)
+                openFileDlg.Filter = "FDF Documents|*.fdf";
+            else
+                openFileDlg.Filter = "XFDF Documents|*.xfdf";
+            if (openFileDlg.ShowDialog().Value)
+            {
+                Syncfusion.Pdf.Parsing.DataFormat dataFormat;
+                if (fdfRadioButton.IsChecked.Value)
+                    dataFormat = Syncfusion.Pdf.Parsing.DataFormat.Fdf;
+                else
+                    dataFormat = Syncfusion.Pdf.Parsing.DataFormat.XFdf;
+                //Import PDF form data
+                pdfViewer.ImportFormData(openFileDlg.FileName, dataFormat);
+            }
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            pdfViewer.Save("Modified.pdf");
+            Microsoft.Win32.SaveFileDialog saveFileDlg = new Microsoft.Win32.SaveFileDialog();
+            if (fdfRadioButton.IsChecked.Value)
+                saveFileDlg.Filter = "FDF Documents|*.fdf";
+            else
+                saveFileDlg.Filter = "XFDF Documents|*.xfdf";
+            if (saveFileDlg.ShowDialog().Value)
+            {
+                Syncfusion.Pdf.Parsing.DataFormat dataFormat;
+                if (fdfRadioButton.IsChecked.Value)
+                    dataFormat = Syncfusion.Pdf.Parsing.DataFormat.Fdf;
+                else
+                    dataFormat = Syncfusion.Pdf.Parsing.DataFormat.XFdf;
+                //Export PDF form data
+                pdfViewer.ExportFormData(saveFileDlg.FileName, dataFormat, "Modified.pdf");
+            }
+        }
+    }
+}
