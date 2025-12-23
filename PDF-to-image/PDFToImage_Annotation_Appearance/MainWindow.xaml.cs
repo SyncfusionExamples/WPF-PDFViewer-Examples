9d8ea7b3eeb3b4e4a0c0edd576bfc92ab97a5acf
lingeshwaran-sf4984 <lingeshwaran.sankar@syncfusion.com>
2025-12-23 19:09:05 +0530

    992847: Added Single project changes

--- PDF-to-image/PDFToImage_Annotation_Appearance/MainWindow.xaml.cs
+++ PDF-to-image/PDFToImage_Annotation_Appearance/MainWindow.xaml.cs
@@ -2,26 +2,11 @@
 using Syncfusion.Pdf.Graphics;
 using Syncfusion.Pdf.Interactive;
 using Syncfusion.PdfToImageConverter;
-using Syncfusion.Windows.PdfViewer;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PDFToImageConverter_Annotation
+namespace PDFToImage_Annotation_Appearance
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -56,15 +41,14 @@ namespace PDFToImageConverter_Annotation
             page.Annotations.Add(freeText);
 
             //Save the document
-            document.Save("../../Output.pdf");
+            document.Save("../../../Output.pdf");
             //Closes the document
             document.Close(true);
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             PdfToImageConverter imageConverter = new PdfToImageConverter();
-            using (FileStream inputStream = new FileStream("../../Output.pdf", FileMode.Open, FileAccess.ReadWrite))
+            using (FileStream inputStream = new FileStream("../../../Output.pdf", FileMode.Open, FileAccess.ReadWrite))
             {
                 imageConverter.Load(inputStream);
                 // Convert the first page (index 0)
