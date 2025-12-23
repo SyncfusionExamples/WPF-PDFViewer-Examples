5144ce098f0f13ec458bd4101bdbcba05e8edb72
Yaminisrisf4389 <yaminisri.m@syncfusion.com>
2025-12-23 13:59:40 +0530

    911054_thumbnail

---
+++ ThumbnailPane/Programmatically-change-thumbnail-zoompercentage/Programatic_thumbnail/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Syncfusion.Windows.PdfViewer;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programatic_thumbnail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            pdfViewer.Load("../../../F#.pdf");
+            pdfViewer.ThumbnailSettings.IsExpanded = true;
+        }
+        private void pdfViewer_DocumentLoaded(object sender, EventArgs args)
+        {
+            Grid LeftToolGrid = pdfViewer.Template.FindName("PART_Grid", pdfViewer) as Grid;
+            if (LeftToolGrid != null)
+            {
+                foreach (var LeftToolChild in LeftToolGrid.Children)
+                {
+                    if (LeftToolChild is ThumbnailPane)
+                    {
+                        StackPanel stackPanel = (LeftToolChild as ThumbnailPane).Template.FindName("Thumb_StackPanel", (LeftToolChild as ThumbnailPane)) as StackPanel;
+                        foreach (var stackPanelChild in stackPanel.Children)
+                        {
+                            if (stackPanelChild is Slider)
+                            {
+                                (stackPanelChild as Slider).Value = 12;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
