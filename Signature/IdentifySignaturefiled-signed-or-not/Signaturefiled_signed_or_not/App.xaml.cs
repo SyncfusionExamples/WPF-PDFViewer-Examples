76a54dff10717052c99b9b9c0f417c45ac41096d
Yaminisrisf4389 <yaminisri.m@syncfusion.com>
2026-04-15 15:32:03 +0530

    1021740

---
+++ Signature/IdentifySignaturefiled-signed-or-not/Signaturefiled_signed_or_not/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Signaturefiled_signed_or_not
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
