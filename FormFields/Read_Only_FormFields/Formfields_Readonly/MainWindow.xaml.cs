d58c5636feacd6a4676f81c69f94a25bcb9d1cbb
Yaminisrisf4389 <yaminisri.m@syncfusion.com>
2026-05-26 10:43:43 +0530

    Readonly_formfields

---
+++ FormFields/Read_Only_FormFields/Formfields_Readonly/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Formfields_Readonly
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<PdfField> user1List = new ObservableCollection<PdfField>();
+        private ObservableCollection<PdfField> user2List = new ObservableCollection<PdfField>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            pdfViewer.Load("../../../Input.pdf");
+            pdfViewer.DocumentLoaded += PdfViewer_DocumentLoaded;
+        }
+
+        private void PdfViewer_DocumentLoaded(object sender, System.EventArgs e)
+        {
+            UpdateFormFields();
+        }
+
+        private void UpdateFormFields()
+        {
+
+            if (pdfViewer.LoadedDocument.Form != null)
+            {
+                var FormFieldCollections = pdfViewer.LoadedDocument.Form.Fields;
+
+                if (FormFieldCollections.Count > 0)
+                {
+                    for (int i = 0; i < FormFieldCollections.Count; i++)
+                    {
+                        if (FormFieldCollections[i].Name == "User1_Name" || FormFieldCollections[i].Name == "User1_Age" || FormFieldCollections[i].Name == "User1_Country")
+                        {
+                            user1List.Add(FormFieldCollections[i]);
+                        }
+                        else
+                        {
+                            user2List.Add(FormFieldCollections[i]);
+                        }
+                    }
+                }
+
+
+
+            }
+        }
+        private void UserComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (pdfViewer.LoadedDocument == null) return;
+
+            var selectedUser = (UserComboBox.SelectedItem as ComboBoxItem).Content.ToString();
+
+            if (selectedUser == "User1")
+            {
+                foreach (var field in user1List)
+                {
+                    if (field is PdfLoadedTextBoxField textField)
+                        textField.ReadOnly = false;
+                }
+
+                foreach (var field in user2List)
+                {
+                    if (field is PdfLoadedTextBoxField textField)
+                        textField.ReadOnly = true;
+                }
+            }
+            else if (selectedUser == "User2")
+            {
+                foreach (var field in user2List)
+                {
+                    if (field is PdfLoadedTextBoxField textField)
+                        textField.ReadOnly = false;
+                }
+
+                foreach (var field in user1List)
+                {
+                    if (field is PdfLoadedTextBoxField textField)
+                        textField.ReadOnly = true;
+                }
+            }
+        }
+    }
+}
